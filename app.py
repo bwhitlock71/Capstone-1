92ba3765dc82cce6c8b9dcc820fdcd947c975df8
Brayton Whitlock <brayton94@outlook.com>
2023-07-14 16:49:06 -0700

    updated search function

--- app.py
+++ app.py
@@ -16,18 +16,18 @@ def home():
 
 # @app.route('/submission')
 # def test_run():
-#     state = request.args.get('state')
-#     url = f'https://api.openbrewerydb.org/breweries?by_state={state}&per_page=5'
+#     city = request.args.get('city')
+#     url = f'https://api.openbrewerydb.org/v1/breweries?by_city={city}&per_page=3'
 #     response = requests.get(url)
 #     brewery_info = response.json()
 #     return render_template('list.html', brewery_info=brewery_info)
 
 
-@app.route('/submission')
+@app.route('/submission', methods=['GET', 'POST'])
 def test_run():
     state = request.args.get('state')
     city = request.args.get('city')
-    url = f'https://api.openbrewerydb.org/breweries?by_city={city}&by_state={state}&per_page=5'
+    url = f'https://api.openbrewerydb.org/v1/breweries?by_city={city}&by_state={state}'
     response = requests.get(url)
     brewery_info = response.json()
     return render_template('list.html', brewery_info=brewery_info)
