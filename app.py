9da01745ddfbcbb5f22ec3b9448f6e12fcd73e87
Brayton Whitlock <brayton94@outlook.com>
2023-07-13 20:32:34 -0700

    Updated search function

--- app.py
+++ app.py
@@ -12,11 +12,22 @@ debug = DebugToolbarExtension(app)
 def home():
     return render_template('home.html')
 
+"""I was able to get the route below this to work correctly but I am having trouble with the second one down. It returns an empty list and I can't seem to get it to work."""
+
+# @app.route('/submission')
+# def test_run():
+#     state = request.args.get('state')
+#     url = f'https://api.openbrewerydb.org/breweries?by_state={state}&per_page=5'
+#     response = requests.get(url)
+#     brewery_info = response.json()
+#     return render_template('list.html', brewery_info=brewery_info)
+
 
 @app.route('/submission')
 def test_run():
     state = request.args.get('state')
-    url = f'https://api.openbrewerydb.org/v1/breweries?by_state={state}'
+    city = request.args.get('city')
+    url = f'https://api.openbrewerydb.org/breweries?by_city={city}&by_state={state}&per_page=5'
     response = requests.get(url)
-    data = response.json()
-    return data
+    brewery_info = response.json()
+    return render_template('list.html', brewery_info=brewery_info)
