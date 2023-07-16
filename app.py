3a03f1a024377fe530611288d7cb652689c15520
Brayton Whitlock <brayton94@outlook.com>
2023-07-16 15:06:53 -0700

    WTForms added to search function

--- app.py
+++ app.py
@@ -10,26 +10,8 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
 
-"""I was able to get the route below this to work correctly but I am having trouble with the second one down. It returns an empty list and I can't seem to get it to work."""
-
-# @app.route('/submission', methods=['GET', 'POST'])
-# def test_run():
-#     state = request.args.get('state')
-#     city = request.args.get('city')
-#     url = f'https://api.openbrewerydb.org/v1/breweries?by_city={city}&by_state={state}'
-#     response = requests.get(url)
-#     if response.status_code != 200:
-#         flash("No results :(", "Please try again")
-#         return render_template('base.html')
-#     brewery_info = response.json()
-#     return render_template('list.html', brewery_info=brewery_info)
-    
-    
-@app.route('/search', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def search_route():
     form = SearchForm()
 
@@ -39,4 +21,7 @@ def search_route():
         url = f'https://api.openbrewerydb.org/v1/breweries?by_city={city}&by_state={state}'
         response = requests.get(url)
         brewery_info = response.json()
-        return render_template('search.html', form=form, brewery_info=brewery_info)
+        return render_template('home.html', form=form, brewery_info=brewery_info)
+    
+
+    return render_template('home.html', form=form)
