01b6a6a260c074e04956a34685e2b926550af986
Brayton Whitlock <brayton94@outlook.com>
2023-07-16 17:00:20 -0700

    Specific form updated

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, SelectField, RadioField
 from wtforms.validators import DataRequired
 
 
@@ -11,4 +11,6 @@ class SearchForm(FlaskForm):
 class SpecificForm(FlaskForm):
     city = StringField('City', validators=[DataRequired()])
     state = StringField('State', validators=[DataRequired()])
-    category = StringField('Type of', validators=[DataRequired()])
+    category = RadioField('Type:', 
+                           choices=[('micro', 'Mirco- Most craft breweries. For example, Samual Adams is still considered a micro brewery.'), ('nano', 'Nano- An extremely small brewery which typically only distributes locally.'), ('brewpub', 'Brewpub- A beer-focused restaurant or restaurant/bar with a brewery on-premise.'), ('bar', 'Bar- A bar. No brewery equipment on premise.')]
+                           )
