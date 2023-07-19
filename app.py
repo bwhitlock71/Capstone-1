42d554204251b111f36c8514fa5ebd3c87db7242
Brayton Whitlock <brayton94@outlook.com>
2023-07-18 20:57:27 -0700

    beginning to add user login

--- app.py
+++ app.py
@@ -1,15 +1,97 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from forms import SearchForm, SpecificForm
+from sqlalchemy.exc import IntegrityError
+
+from forms import SearchForm, SpecificForm, UserAddForm, LoginForm
+from models import db, connect_db, User, Brewery, Reviews
+
 import requests
 
+CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "icanttellyou"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
 
 debug = DebugToolbarExtension(app)
 
+@app.before_request
+def add_user_to_g():
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+    else:
+        g.user = None
+
+
+def do_login(user):
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+            )
+            db.session.commit()
+
+        except IntegrityError as e:
+            flash("Username already taken", 'danger')
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit(): 
+        user = User.authenticate(form.username.data, 
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+
+    do_logout()
+
+    flash("You have logged out.")
+    return redirect("/login")
 
 @app.route('/', methods=['GET', 'POST'])
 def search_route():
